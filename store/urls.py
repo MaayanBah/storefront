98ac72d7f99103cf8e52ba83f47dec7f2ce611f1
maayan <maayan@gmail.com>
2024-12-18 18:53:25 +0200

    Add Cart and CartItem functionality with models, serializers, views, and router

--- store/urls.py
+++ store/urls.py
@@ -8,9 +8,16 @@ from . import views
 router = routers.DefaultRouter()
 router.register("products", views.ProductViewSet, basename="products")
 router.register("collections", views.CollectionViewSet)
+router.register(
+    "carts",
+    views.CartViewSet,
+)
 
 product_router = routers.NestedDefaultRouter(router, "products", lookup="product")
 product_router.register("reviews", views.ReviewViewSet, basename="product-reviews")
 
+carts_router = routers.NestedDefaultRouter(router, "carts", lookup="cart")
+carts_router.register("items", views.CartItemViewSet, basename="cart-items")
 
-urlpatterns = router.urls + product_router.urls
+
+urlpatterns = router.urls + product_router.urls + carts_router.urls
