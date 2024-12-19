635bd75bad6456551dace13063385c5e78efea5a
maayan <maayan@gmail.com>
2024-12-19 13:50:28 +0200

    Update user model, add order permissions and rename app

--- storefront/settings.py
+++ storefront/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     "playground",
     "debug_toolbar",
     "store",
-    "store_custom",
     "tags",
     "likes",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -91,7 +91,7 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "storefront",
+        "NAME": "storefront2",
         "HOST": "localhost",
         "USER": "root",
         "PASSWORD": "dgjxfgh3%A",
@@ -147,3 +147,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
+
+AUTH_USER_MODEL = "core.User"
