2f1503f889795aad69688be7bdfd520771f6c4a3
maayan <maayan@gmail.com>
2024-12-22 18:25:39 +0200

    Create custom signals

--- store/serializers.py
+++ store/serializers.py
@@ -11,6 +11,7 @@ from .models import (
     Order,
     OrderItem,
 )
+from .signals import order_created
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -190,4 +191,6 @@ class CreateOrderSerializer(serializers.Serializer):
 
             OrderItem.objects.bulk_create(order_items)
             Cart.objects.filter(pk=cart_id).delete()
+
+            order_created.send_robust(sender=self.__class__, order=order)
             return order
