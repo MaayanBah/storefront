e48a3b3f101fb1cb6740b615fcc8f0e2ced98cd4
maayan <maayan@gmail.com>
2024-12-22 18:10:19 +0200

    Fix the violation of Command Query Separation (CQS) principle by using signals and create customer when user is created

--- store/views.py
+++ store/views.py
@@ -126,7 +126,7 @@ class CustomerViewSet(ModelViewSet):
 
     @action(detail=False, methods=["GET", "PUT"], permission_classes=[IsAuthenticated])
     def me(self, request):
-        (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+        customer = Customer.objects.get(user_id=request.user.id)
         if request.method == "GET":
             serializer = CustomerSerializer(customer)
         if request.method == "PUT":
@@ -178,7 +178,5 @@ class OrderViewSet(ModelViewSet):
             print("q: ", Order.objects.all())
             return Order.objects.all()
 
-        (customer_id, created) = Customer.objects.only("id").get_or_create(
-            user_id=user.id
-        )
+        customer_id = Customer.objects.only("id").get(user_id=user.id)
         return Order.objects.filter(customer_id=customer_id)
