af41ffd738eb3cd8b74a78d19acf78df3ab01ef4
maayan <maayan@gmail.com>
2024-12-23 15:55:17 +0200

    Add option to get images from the API, and implement file size validation for uploaded image files

---
+++ store/validators.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+
+MAX_SIZE_KB = 50
+
+
+def validate_file_size(file):
+    if file.size > MAX_SIZE_KB * 1024:
+        raise ValidationError(f"Files can't be larger than {MAX_SIZE_KB}KB")
