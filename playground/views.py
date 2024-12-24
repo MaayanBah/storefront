34c56f6a207b945c93ba28988c2878da4fa9f6bc
maayan <maayan@gmail.com>
2024-12-24 11:27:27 +0200

    Add option to send email

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.core.mail import BadHeaderError
+from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
+    try:
+        message = BaseEmailMessage(
+            template_name="emails/hello.html", context={"name": "Maayan"}
+        )
+        message.send(["to@gmail.com"])
+    except BadHeaderError:
+        pass
     return render(request, "hello.html", {"name": "Mosh"})
