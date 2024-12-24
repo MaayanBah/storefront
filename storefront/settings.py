90c437b497597febd3e430a799e448a2f0bb7164
maayan <maayan@gmail.com>
2024-12-24 12:19:30 +0200

    Set up Celery

--- storefront/settings.py
+++ storefront/settings.py
@@ -169,3 +169,11 @@ DJOSER = {
 }
 
 SIMPLE_JWT = {"AUTH_HEADER_TYPES": ("JWT",), "ACCESS_TOKEN_LIFETIME": timedelta(days=1)}
+
+EMAIL_HOST = "localhost"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = "admin@gmail.com"
+
+CELERY_BROKER_URL = "redis://localhost:6379/1"
